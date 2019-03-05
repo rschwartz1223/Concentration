518c877a7baf47585d0d142da39e80d92e9809ab
rschwartz1223 <rschwartz1223@gmail.com>
2019-03-05 14:48:37 -0800

    Created MVC, added card shuffle

---
+++ Concentration/Card.swift
@@ -0,0 +1,29 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Robert Schwartz on 3/5/19.
+//  Copyright © 2019 Robert Schwartz. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int
+    {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init()
+    {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,60 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Robert Schwartz on 3/5/19.
+//  Copyright © 2019 Robert Schwartz. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int)
+    {
+        if !cards[index].isMatched
+        {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index
+            {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier
+                {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            }
+            else
+            {
+                //either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices
+                {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int)
+    {
+        for _ in 1...numberOfPairsOfCards
+        {
+            let card = Card()
+            cards += [card, card]
+        }
+        //Shuffle the cards
+        for number in 1...cards.count
+        {
+            let randomIndex = Int(arc4random_uniform(UInt32(cards.count - number)))
+            cards.append(cards.remove(at: randomIndex))
+        }
+    }
+}
+
