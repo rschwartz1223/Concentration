7f029e08621dc819cef52a70462a4f28666bd0a5
rschwartz1223 <rschwartz1223@gmail.com>
2019-03-18 14:53:16 -0700

    implemented hashable protocol for Card class

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -50,7 +50,7 @@ struct Concentration
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index
             {
                 //check if cards match
-                if cards[matchIndex].identifier == cards[index].identifier
+                if cards[matchIndex] == cards[index]
                 {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
